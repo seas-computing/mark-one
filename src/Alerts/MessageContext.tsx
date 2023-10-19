945afc7176ed36b0ce6c3742663ae3e15be68ac7
urji h <urjig122@gmail.com>
2023-10-19 16:46:32 -0400

    Added Message_Action and AppMessage class and type to define the case on MessageReducer To fix type error on messageReducer, i added bothe Message_Action and AppMessage classes from the react boilerplate. Initially i was going to re-write(re-define perhaps) the Message_Act ion However to keep consistency across the apps, i brought both definitions from the react boilerplate.

--- src/Alerts/MessageContext.tsx
+++ src/Alerts/MessageContext.tsx
@@ -1,15 +1,73 @@
-import { createContext, Context, Reducer } from 'react';
-
+import { createContext, Reducer } from 'react';
 /**
  * Global message provider
  */
 
-export const MessageContext: Context<Function> = createContext(null);
+export const MessageContext = createContext(null);
+
+/**
+ * Possible types of message to display to the user
+ */
+
+export enum MESSAGE_TYPE {
+  ERROR = 'ERROR',
+  SUCCESS = 'SUCCESS',
+  INFO = 'INFO',
+}
+
+/**
+ * Possible actions to take on the message queue
+ */
+
+export enum MESSAGE_ACTION {
+  PUSH = 'PUSH',
+  CLEAR = 'CLEAR',
+}
+
+/**
+ * Used for displaying messages on the client
+ */
+
+export class AppMessage {
+  public readonly variant: MESSAGE_TYPE;
+
+  public readonly text: string;
+
+  /**
+   * Create a new application message. By default, sets the message priority to
+   * "info", but alternativae values can be passed in as the second parameter.
+   */
+
+  public constructor(
+    message: string,
+    variant: MESSAGE_TYPE = MESSAGE_TYPE.INFO
+  ) {
+    this.variant = variant;
+    this.text = message;
+  }
+}
 
 /**
  * handles queueing logic for the top-level app component
  */
 
+/**
+ * Defines the state of the message reducer
+ */
+
+export interface MessageReducerState {
+  queue: AppMessage[];
+  currentMessage: AppMessage;
+}
+
+/**
+ * Defines that kinds of actions that can be accepted by the reducer
+ */
+
+export interface MessageReducerAction {
+  type: MESSAGE_ACTION;
+  message?: AppMessage;
+}
 
 export const messageReducer:
 Reducer<MessageReducerState, MessageReducerAction> = (
@@ -18,7 +76,7 @@ Reducer<MessageReducerState, MessageReducerAction> = (
 ): MessageReducerState => {
   const { currentMessage, queue } = state;
   switch (action.type) {
-    case (): {
+    case (MESSAGE_ACTION.PUSH): {
       if (!currentMessage) {
         return ({
           ...state,
@@ -31,7 +89,7 @@ Reducer<MessageReducerState, MessageReducerAction> = (
         queue: newQueue,
       };
     }
-    case (''): {
+    case (MESSAGE_ACTION.CLEAR): {
       const nextQueue = [...queue];
       const nextMessage = nextQueue.shift();
       return {
