749406493c42b5efd686befb41f81760c6c2f750
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2024-02-15 15:56:20 -0500

    Remove withTheme to fix type issues
    To fix the type issue "Type '{ children: string; }' has no properties in common with type 'IntrinsicAttributes & ExecutionProps & RefAttributes<IStyledComponent<"web"", we removed the withTheme call. It seems that the theme is already integrated.

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,14 +1,12 @@
-import { ReactElement, ReactNode } from 'react';
-import styled, { withTheme } from 'styled-components';
+import { PropsWithChildren, ReactElement } from 'react';
+import styled from 'styled-components';
 
-export interface HeaderProps {
+export interface HeaderProps extends PropsWithChildren {
   /**
    * Allows for applying a custom background color
    * @default "transparent"
    * */
   background?: string;
-  /** The elements to be displayed inside the Header */
-  children: ReactNode;
   /**
    * Pass in a custom value for justify-content
    * @default space-between
@@ -20,7 +18,7 @@ export interface HeaderProps {
  * A full-width header to be displayed at the top of the page
  */
 
-const Header = styled.header<HeaderProps>`
+const Header = styled.header<PropsWithChildren<HeaderProps>>`
   align-items: baseline;
   background-color: ${({ background }) => background};
   display: flex;
@@ -35,4 +33,4 @@ Header.defaultProps = {
 };
 
 declare type Header = ReactElement<HeaderProps>;
-export default withTheme(Header);
+export default Header;
