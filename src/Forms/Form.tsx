b83af14916f4dcb89c2b1fbeefc1f4c432ac4caa
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2024-03-20 15:45:19 -0400

    Fix Form component
    When trying to use the Form component in the makerspace project, we get the following error message: "Type '{ children: Element[]; id: string; label: string; submitHandler: () => void; }' is not assignable to type 'IntrinsicAttributes & FormProps'.
      Property 'children' does not exist on type 'IntrinsicAttributes & FormProps'."
    This issue was fixed in the past for mark one components by explicitly passing down a children prop.

--- src/Forms/Form.tsx
+++ src/Forms/Form.tsx
@@ -1,33 +1,51 @@
-import {
+import React, {
   FunctionComponent,
   FormEventHandler,
   FormEvent,
+  ReactNode,
+  ReactElement,
 } from 'react';
 import styled from 'styled-components';
 import { fromTheme } from '../Theme';
 
 export interface FormProps {
   /** The id of the form */
-  id?: string;
+  id: string;
   /** A label that specifies the purpose of the form */
   label: string;
   /** Handler attached to the onSubmit handler */
   submitHandler?: FormEventHandler<HTMLFormElement>
+  /** The form elements */
+  children: ReactNode[];
 }
 
-const Form: FunctionComponent<FormProps> = styled.form.attrs<FormProps>(
-  (props: FormProps) => ({
-    id: props.id,
-    'aria-label': props.label,
-    onSubmit: (e: FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      props.submitHandler(e);
-    },
-  })
-)`
-  & > * {
-    margin: ${fromTheme('ws', 'small')};
-  }
+const StyledForm = styled.form`
+  margin: ${fromTheme('ws', 'small')};
 `;
+
+const Form: FunctionComponent<FormProps> = (props): ReactElement => {
+  const {
+    id,
+    label,
+    submitHandler,
+    children,
+  } = props;
+  return (
+    <StyledForm
+      id={id}
+      aria-label={label}
+      onSubmit={(e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        submitHandler(e);
+      }}
+    >
+      {children}
+    </StyledForm>
+  );
+};
+
+Form.defaultProps = {
+  submitHandler: () => {},
+};
 
 export default Form;
