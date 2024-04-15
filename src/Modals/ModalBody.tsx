ab8bf2faa1a0d44f829df5b5a14b423d1d555108
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2024-04-15 11:58:58 -0400

    Declare ModalBody and ModalFooter types
    PropsWithChildren was included within the definition of the types for the ModalBody and ModalFooter. These changes are in response to getting a type error message when trying to use these components in the Makerspace project related to the "children" prop. (Type '{ children: Element; }' has no properties in common with type 'IntrinsicAttributes & ExecutionProps & RefAttributes<IStyledComponent<"web", Substitute<DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>, { ...; }>>>'.ts(2559)

--- src/Modals/ModalBody.tsx
+++ src/Modals/ModalBody.tsx
@@ -1,14 +1,16 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, ReactElement } from 'react';
 import styled, { withTheme } from 'styled-components';
 
-const StyledModalBody = styled.div<PropsWithChildren>`
+const ModalBody = styled.div`
   padding: ${({ theme }): string => theme.ws.medium};
   overflow: auto;
 `;
+
+declare type ModalBody = ReactElement<PropsWithChildren>;
 
 /**
  * @component ModalBody
  * Used within the Modal component to provide appropriate spacing in line with
  * the ModalHeader and ModalFooter components.
  */
-export default withTheme(StyledModalBody);
+export default withTheme(ModalBody);
--- src/Modals/ModalFooter.tsx
+++ src/Modals/ModalFooter.tsx
@@ -1,7 +1,7 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, ReactElement } from 'react';
 import styled, { withTheme } from 'styled-components';
 
-const styledModalFooter = styled.div<PropsWithChildren>`
+const ModalFooter = styled.div`
   display: flex;
   flex-direction: row-reverse;
   justify-content: space-between;
@@ -9,9 +9,11 @@ const styledModalFooter = styled.div<PropsWithChildren>`
   width: 100%;
 `;
 
+declare type ModalFooter = ReactElement<PropsWithChildren>;
+
 /**
  * @component ModalFooter
  * Used within the Modal component to render a separated bottom section,
  * typically containing buttons for save, cancel, etc.
  */
-export default withTheme(styledModalFooter);
+export default withTheme(ModalFooter);
