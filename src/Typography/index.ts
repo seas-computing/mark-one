cc15f4b71aac733c675e04f6e7eef70aada75636
Amalya Megerman <amalyamegerman@gmail.com>
2024-08-09 11:06:40 -0400

    Add SectionSubHeading component

--- src/Typography/index.ts
+++ src/Typography/index.ts
@@ -1,3 +1,4 @@
 export { default as PageTitle } from './PageTitle';
 export { default as SectionHeading } from './SectionHeading';
 export { default as NoteText } from './NoteText';
+export { default as SectionSubHeading } from './SectionSubHeading';
