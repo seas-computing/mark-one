36df293f48f9bfb3ad328472970e85c7879540eb
Amalya Megerman <amalyamegerman@gmail.com>
2024-08-22 11:26:08 -0400

    undo prettier reformatting

--- src/Forms/index.ts
+++ src/Forms/index.ts
@@ -1,10 +1,6 @@
 export { default as ValidationErrorMessage } from './ValidationErrorMessage';
 export { default as TextInput } from './TextInput';
-export {
-  default as Dropdown,
-  DropdownProps,
-  DropdownOptionProps,
-} from './Dropdown';
+export { default as Dropdown, DropdownProps, DropdownOptionProps } from './Dropdown';
 export { default as Checkbox } from './Checkbox';
 export { default as Combobox } from './Combobox';
 export { default as InputLabel, POSITION } from './InputLabel';
