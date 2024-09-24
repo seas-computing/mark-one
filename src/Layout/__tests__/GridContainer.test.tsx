88104dcbff8db70e6b85aca123a517843654198d
Amalya Megerman <amalyamegerman@gmail.com>
2024-09-24 10:20:08 -0400

    add multiple assertions for each test

--- src/Layout/__tests__/GridContainer.test.tsx
+++ src/Layout/__tests__/GridContainer.test.tsx
@@ -2,17 +2,25 @@ import { strictEqual } from 'assert';
 import { getGridColumnStyles } from '../GridContainer';
 
 describe('getGridColumnStyles function', function () {
-  it('should return the correct grid-column value for placement "left"', function () {
-    const result = getGridColumnStyles('left', 12);
-    strictEqual(result, 'span 12');
+  it('returns correct grid-column value for placement "left"', function () {
+    strictEqual(getGridColumnStyles('left', 1), 'span 1');
+    strictEqual(getGridColumnStyles('left', 7), 'span 7');
+    strictEqual(getGridColumnStyles('left', 12), 'span 12');
   });
-  it('should return the correct grid-column value for placement "center"', function () {
-    const result = getGridColumnStyles('center', 4);
-    strictEqual(result, '5 / span 4');
+  it('returns correct grid-column value for placement "center"', function () {
+    strictEqual(getGridColumnStyles('center', 1), '6 / span 1');
+    strictEqual(getGridColumnStyles('center', 7), '3 / span 7');
+    strictEqual(getGridColumnStyles('center', 12), '1 / span 12');
   });
-  it('should return the correct grid-column value for placement "right"', function () {
-    const result = getGridColumnStyles('right', 6);
-    strictEqual(result, '7 / span 6');
+  it('returns correct grid-column value for placement "right"', function () {
+    strictEqual(getGridColumnStyles('right', 1), '12 / span 1');
+    strictEqual(getGridColumnStyles('right', 7), '6 / span 7');
+    strictEqual(getGridColumnStyles('right', 12), '1 / span 12');
+  });
+  it('returns the correct grid-column value for numbered placements', function () {
+    strictEqual(getGridColumnStyles(4, 12), '4 / span 12');
+    strictEqual(getGridColumnStyles(7, 6), '7 / span 6');
+    strictEqual(getGridColumnStyles(9, 5), '9 / span 5');
   });
   it('should return the correct grid-column value for numbered placements', function () {
     const result = getGridColumnStyles(7, 6);
